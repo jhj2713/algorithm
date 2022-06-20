5048dc47702a263a37a2c971b4e924240ed3718d
juhyojeong <juhyojeong58@gmail.com>
2022-06-20 21:31:58 +0900

    Baekjoon: 11052(fix)

--- Baekjoon/DP/11052.js
+++ Baekjoon/DP/11052.js
@@ -6,7 +6,6 @@ let arr = input[1].split(" ").map((str) => Number(str));
 let dp = [0, ...arr];
 
 for (let i = 2; i <= n; i++) {
-  dp.push(arr[i - 1]);
   for (let j = 1; j < i; j++) {
     dp[i] = Math.max(dp[i - j] + arr[j - 1], dp[i]);
   }
