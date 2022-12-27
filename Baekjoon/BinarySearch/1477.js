0a86ee3a8e0c2132660e8fcf02249a4178e4ead1
juhyojeong <juhyojeong58@gmail.com>
2022-12-27 11:16:03 +0900

    Baekjoon: 1477(fix)

--- Baekjoon/BinarySearch/1477.js
+++ Baekjoon/BinarySearch/1477.js
@@ -1,12 +1,10 @@
-const input = `4 5 10
-2 4 6 8`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const [n, m, l] = input[0].split(" ").map(Number);
-let position = input[1].split(" ").map(Number);
+const [n, m, l] = input[0].trim().split(" ").map(Number);
+let position = input[1].trim().split(" ").map(Number);
 position.push(0, l);
 position = position.sort((a, b) => a - b);
-
-console.log(Math.floor(0 / 4));
 
 let left = 1,
   right = l,
